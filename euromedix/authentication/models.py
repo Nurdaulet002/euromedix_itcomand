b7e0a8572337d74ee37bc8f7cb59233d940bb84b
Nurdaulet002 <73096695+Nurdaulet002@users.noreply.github.com>
2021-01-05 15:45:05 +0600

    ss

--- euromedix/authentication/models.py
+++ euromedix/authentication/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from sur.models import Hospital, FuncStructure, Specialtie
 
-# Create your models here.
+# Таблица производных пользователей
+class User(AbstractUser):
+    hospital = models.ForeignKey(
+        Hospital, on_delete=models.CASCADE, default="1")
+    funcStructure = models.ForeignKey(
+        FuncStructure, on_delete=models.CASCADE, default="1")
+    specialty = models.ForeignKey(
+        Specialtie, on_delete=models.CASCADE, default="1")
+
+    @property
+    # получить ФИО доктора
+    def full_name(self):
+        return self.last_name + ' ' + self.first_name
--- euromedix/authentication/views.py
+++ euromedix/authentication/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 # Create your views here.
+
+
+def identifyRole(request):
+    if(request.user.role.title == 'doctor'):
+        return redirect('doctorPanel:index')
+    elif(request.user.role.title == 'registration'):
+        return redirect('registrationPanel:index')
